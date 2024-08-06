4d8a0ffb374b4806be8e2939a5d097d418b7882b
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2024-08-06 07:44:22 +0200

    Basic reporting bookbot

---
+++ main.py
@@ -0,0 +1,51 @@
+
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    file_contents = get_book_text(path_to_file)
+    file_word_count = get_word_count(file_contents)
+    char_counts = get_char_count(file_contents)
+    print_report(path_to_file, file_word_count, char_counts)
+
+def get_word_count(in_string : str) -> int:
+    words = in_string.split()
+    return len(words)
+
+def get_char_count(in_string: str) -> dict:
+    chars = dict()
+    for c in in_string:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def char_list_sort(in_dict:dict):
+    return in_dict["count"]
+
+def convert_char_dict_to_list(in_dict:dict)->list:
+    out_list = []
+    for key in in_dict:
+        out_list.append({"char":key, "count":in_dict[key]})
+    out_list.sort(reverse=True, key=char_list_sort)
+    return out_list
+
+def print_report(file_path:str, word_count:int, chars_dict:dict)->None:
+    print(f"--- Begining report of {file_path} ---")
+    print(f"{word_count} words found in document")
+    print()
+    char_list = convert_char_dict_to_list(chars_dict)
+    for item in char_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The {item["char"]} character was found {item["count"]} times")
+    print("--- End report ---")
+    return
+
+def get_book_text(file_path : str) -> str:
+    with open(file_path,"r") as fp:
+        return fp.read()
+
+if __name__ == "__main__":
+    main()
