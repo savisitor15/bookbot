829b6315d0fc637f592f8dbe22eae644fb354cd7
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2025-02-27 18:58:17 +0200

    updated project to be in aligment to boot.dev updates

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-
+from stats import get_word_count
+import sys
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+    path_to_file = sys.argv[1]
     file_contents = get_book_text(path_to_file)
     file_word_count = get_word_count(file_contents)
     char_counts = get_char_count(file_contents)
     print_report(path_to_file, file_word_count, char_counts)
-
-def get_word_count(in_string : str) -> int:
-    words = in_string.split()
-    return len(words)
 
 def get_char_count(in_string: str) -> dict:
     chars = dict()
@@ -33,13 +32,13 @@ def convert_char_dict_to_list(in_dict:dict)->list:
 
 def print_report(file_path:str, word_count:int, chars_dict:dict)->None:
     print(f"--- Begining report of {file_path} ---")
-    print(f"{word_count} words found in document")
+    print(f"{word_count} words found in the document")
     print()
     char_list = convert_char_dict_to_list(chars_dict)
     for item in char_list:
         if not item["char"].isalpha():
             continue
-        print(f"The {item["char"]} character was found {item["count"]} times")
+        print(f"{item["char"]}: {item["count"]} times")
     print("--- End report ---")
     return
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+
+def get_word_count(in_string : str) -> int:
+    words = in_string.split()
+    return len(words)
